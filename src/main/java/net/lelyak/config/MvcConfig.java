df25401085faf6e44ac0394379140aa48cff403b
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-09-07 19:22:37 +0300

    add uploading files functionality

--- src/main/java/net/lelyak/config/MvcConfig.java
+++ src/main/java/net/lelyak/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package net.lelyak.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,11 +12,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/").setViewName("main");
-//        registry.addViewController("/registration").setViewName("registration");
+//        registry.addViewController("/").setViewName("main");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations(String.format("file://%s/", uploadPath));
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
--- src/main/java/net/lelyak/controller/MainController.java
+++ src/main/java/net/lelyak/controller/MainController.java
@@ -1,27 +1,35 @@
 package net.lelyak.controller;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import net.lelyak.domain.Message;
 import net.lelyak.domain.Role;
 import net.lelyak.domain.User;
 import net.lelyak.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * @author Nazar Lelyak.
  */
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MainController {
 
     private final MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -52,13 +60,30 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model
-    ) {
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+            ) throws IOException {
         Message message = Message.builder()
                 .text(text)
                 .tag(tag)
                 .author(user)
                 .build();
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = String.format("%s.%s", uuidFile, file.getOriginalFilename());
+
+            file.transferTo(new File(String.format("%s/%s", uploadPath, resultFileName)));
+
+            message.setFilename(resultFileName);
+        }
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
