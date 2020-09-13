b271522e2db15533f875c35f128ad975cbb7042e
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-09-13 14:58:09 +0300

    updates for deployment on Unix server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,17 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/id_rsa_drucoder \
+scp -i ~/.ssh/id_rsa \
     target/sweater-1.0-SNAPSHOT.jar \
     dru@192.168.0.107:/home/dru/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa_drucoder dru@192.168.0.107 << EOF
+ssh -i ~/.ssh/id_rsa dru@192.168.0.107 << EOF
+
 pgrep java | xargs kill -9
-nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
+nohup java -jar -Dgmail-account-name=... -Dgmail-account-pass=... -Drecaptcha.secret=...  sweater-1.0.jar > sweater_log.txt &
+
 EOF
 
 echo 'Bye'
