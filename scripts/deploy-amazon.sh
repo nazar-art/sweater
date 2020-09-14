1b06901a7f901a50156b850d97164fe2740bca9e
Nazar lelyak <nazar.lelyak@globallogic.com>
2020-09-14 19:29:04 +0300

    update deploy-amazon.sh

--- scripts/deploy-amazon.sh
+++ scripts/deploy-amazon.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+USER='ec2-user'
+HOSTNAME='ec2-18-194-243-47.eu-central-1.compute.amazonaws.com'
+
+echo 'Build jar:'
 mvn clean package
 
-echo 'Copy files...'
-
+echo 'Copy files:'
 scp -i ~/.ssh/sweater-frankfurt.pem \
     target/sweater-1.0.jar \
-    ec2-user@ec2-18-194-243-47.eu-central-1.compute.amazonaws.com:/home/ec2-user/
+    USER@HOSTNAME:/home/ec2-user/
 
-echo 'Restart server...'
-
-ssh -i ~/.ssh/sweater-frankfurt.pem ec2-user@ec2-18-194-243-47.eu-central-1.compute.amazonaws.com << EOF
+echo 'Restart server:'
+ssh -i ~/.ssh/sweater-frankfurt.pem USER@HOSTNAME << EOF
 pgrep java | xargs kill -9
 nohup java -jar sweater-1.0.jar > log.txt &
 EOF
